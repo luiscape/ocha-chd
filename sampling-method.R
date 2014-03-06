fb8dd1a462086f29e4f529f8619d69c5ec39691f
Luis Capelo <luiscape@gmail.com>
2014-03-06 07:23:32 -0500

    Trying to fix the sampling function.

--- sampling-method.R
+++ sampling-method.R
@@ -2,34 +2,38 @@
 # Systematic sampling method on a data.frame.
 
 rw.sample <- function(df = NULL, n = NULL) {
-  
   # Creating an `id` column. 
-  a <- data.frame(1)
-  colnames(a)[1] <- "id"
-  
-  # Adding each number to a column.
-  for (i in 1:nrow(df)) { 
-    x <- i
-    a <- rbind(a, x)
+  id.creator <- function(df = NULL) {
+    a <- data.frame(1)
+    for (i in 1:nrow(df)) { 
+      x <- i
+      a <- rbind(a, x)
+    }
+    a <- data.frame(a[1:nrow(a)-1,])
+    df <- cbind(a, df)
+    colnames(df)[1] <- "id"
+    return(df)
   }
-  
-  # Joining the resulting column with the df provided.
-  df <- cbind(a, df)
+  df <- id.creator()
   
   # Function for sampling. 
-  x <- as.numeric(df$id[sample(nrow(df), 1)])
-  for (i in 2:n) {
-    if (((x + n) > nrow(df)) == FALSE) {
-      x <- x + n
+  sampler <- function(df = NULL) {
+    a <- data.frame(1)
+    x <- as.numeric(df$id[sample(nrow(df), 1)])
+    for (i in 2:n) {
+      if (((x + n) > nrow(df)) == FALSE) {
+        x <- x + n
+      }
+      if (((x + n) > nrow(df)) == TRUE) {
+        x <- ((x + n) - nrow(df))
+      }
+      a <- rbind(a, x)
     }
-    if (((x + n) > nrow(df)) == TRUE) {
-      x <- ((x + n) - nrow(df))
-    }
-    a <- rbind(a, x)
+    a <- data.frame(a)
+    colnames(a)[1] <- "id"
+    a <- merge(df, a)
+    return(a)
   }
-  a <- data.frame(a)
-  colnames(a)[1] <- "id"
-  a <- merge(df, a)
+  a <- sampler(df = df)
   return(a)
 }
-
