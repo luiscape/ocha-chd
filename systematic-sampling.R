4d9e52ce603fd90b6b78b2e74ed8a5e43f3cb701
Luis Capelo <luiscape@gmail.com>
2014-03-06 11:21:32 -0500

    Changing the interval method.

--- systematic-sampling.R
+++ systematic-sampling.R
@@ -1,5 +1,5 @@
-############### Sampling method. ################## 
-# Systematic sampling method on a data.frame.     # 
+############### Sampling method. ##################
+# Systematic sampling method on a data.frame.     #
 # n (sample size) has to provided as a parameter. #
 #                                                 #
 # Author: Luis Capelo | capelo@un.org             #
@@ -7,53 +7,55 @@
 ###################################################
 
 rw.sample <- function(df = NULL, n = NULL) {
-  # Creating an `id` column. 
+
+N <- nrow(df)
+int <- (N / n)
+
+  # Creating an `id` column.
   id.creator <- function(df = NULL) {
     print("Creatign 'id' columns -- using a highly inefficient method.")
-    
+
     # Create progress bar.
-    total <- nrow(df)
-    pb <- txtProgressBar(min = 0, max = total, style = 3)
-    
+    pb <- txtProgressBar(min = 0, max = N, style = 3)
+
     a <- data.frame(1)
-    for (i in 1:nrow(df)) { 
+    for (i in 1:nrow(df)) {
       Sys.sleep(0.1)
-      
+
       # Update progress bar.
       setTxtProgressBar(pb, i)
-      
+
       x <- i
       a <- rbind(a, x)
     }
     close(pb)
-    a <- data.frame(a[1:nrow(a)-1,])
+    a <- data.frame(a[1:nrow(a) - 1,])
     df <- cbind(a, df)
     colnames(df)[1] <- "id"
     return(df)
   }
   df <- id.creator(df = df)
-  
-  # Function for sampling. 
+
+  # Function for sampling.
   sampler <- function(df = NULL) {
     print("Sampling.")
-    
+
     # Create progress bar.
-    total <- nrow(df)
-    pb <- txtProgressBar(min = 0, max = total, style = 3)
-    
+    pb <- txtProgressBar(min = 0, max = N, style = 3)
+
     a <- data.frame()
     x <- as.numeric(df$id[sample(nrow(df), 1)])
     for (i in 1:n) {
       Sys.sleep(0.1)
-      
+
       # Update progress bar.
       setTxtProgressBar(pb, i)
-      
-      if (((x + n) > nrow(df)) == FALSE) {
-        x <- x + n
+
+      if (((x + int) > nrow(df)) == FALSE) {
+        x <- x + int
       }
-      if (((x + n) > nrow(df)) == TRUE) {
-        x <- ((x + n) - nrow(df))
+      if (((x + int) > nrow(df)) == TRUE) {
+        x <- ((x + int) - nrow(df))
       }
       a <- rbind(a, x)
     }
